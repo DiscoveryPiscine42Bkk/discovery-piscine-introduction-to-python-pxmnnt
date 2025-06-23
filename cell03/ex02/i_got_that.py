c86b7d067b7598c7763aec603cb2de0cff9e6215
pxmnnt <pxmn.nt@gmail.com>
2025-06-23 08:14:28 +0000

    fixCell03Ex02

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -4,6 +4,8 @@ firstTime = True
 while(True):
     if(firstTime):
         first_time_input = input("What you gotta say?: ")
+        if(first_time_input == "STOP"):
+            break
         firstTime = False
     else:
         repeated_input = input("I got that! Anything else? : ")
